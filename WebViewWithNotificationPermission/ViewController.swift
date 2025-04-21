ea02f7d4f7958a6973c4031d7bb91a40a98496fc
Samar Khaled <samar.khaled@gmail.com>
2025-04-21 15:07:13 +0200

    Adding WKWebView

--- WebViewWithNotificationPermission/ViewController.swift
+++ WebViewWithNotificationPermission/ViewController.swift
@@ -6,14 +6,62 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+    
+    // MARK: - Properties
+
+    private lazy var webView: WKWebView = {
+        let webConfiguration = WKWebViewConfiguration()
+        webConfiguration.applicationNameForUserAgent = "user agent"
+        let webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.uiDelegate = self
+        webView.navigationDelegate = self
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        return webView
+    }()
+
+    private lazy var activityIndicator: UIActivityIndicatorView = {
+        let activityIndicator = UIActivityIndicatorView(style: .large)
+        activityIndicator.hidesWhenStopped = true
+        return activityIndicator
+    }()
+
+    // MARK: - LifeCycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupUI()
+        configureWebView(urlString: "https://www.google.com")
+        activityIndicator.center = view.center
+        view.addSubview(activityIndicator)
     }
 
+    // MARK: - Helpers
 
+    private func setupUI() {
+        navigationController?.setNavigationBarHidden(true, animated: false)
+        view.addSubview(webView)
+        webView.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor).isActive = true
+        webView.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor).isActive = true
+        webView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        webView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+    }
+
+    /// webView configuration
+    private func configureWebView(urlString: String) {
+        guard let url = URL(string: urlString) else { return }
+        webView.load(URLRequest(url: url))
+        let contentController = webView.configuration.userContentController
+        contentController.add(self, name: "toggleMessageHandler")
+    }
 }
 
+extension ViewController: WKScriptMessageHandler {
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {}
+}
+
+// MARK: - WKNavigationDelegate, WKUIDelegate
+
+extension ViewController: WKNavigationDelegate, WKUIDelegate {}
