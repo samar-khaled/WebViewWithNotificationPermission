f8d5e2407d360379010d131ea1fe13ff613f4eeb
Samar Khaled <samar.khaled@gmail.com>
2025-04-21 16:07:45 +0200

    Ask For notification permission from html Java script button

--- WebViewWithNotificationPermission/ViewController.swift
+++ WebViewWithNotificationPermission/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 import WebKit
 
 final class ViewController: UIViewController {
-    
+
     // MARK: - Properties
 
     private lazy var webView: WKWebView = {
@@ -33,7 +33,7 @@ final class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
-        configureWebView(urlString: "https://www.google.com")
+        configureWebView()
         activityIndicator.center = view.center
         view.addSubview(activityIndicator)
     }
@@ -49,17 +49,44 @@ final class ViewController: UIViewController {
         webView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
     }
 
-    /// webView configuration
-    private func configureWebView(urlString: String) {
-        guard let url = URL(string: urlString) else { return }
+    private func configureWebView() {
+        guard let url = Bundle.main.url(forResource: "NotificationPage", withExtension: "html") else { return }
         webView.load(URLRequest(url: url))
         let contentController = webView.configuration.userContentController
-        contentController.add(self, name: "toggleMessageHandler")
+        contentController.add(self, name: "observer")
     }
 }
 
 extension ViewController: WKScriptMessageHandler {
-    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {}
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { [weak self] granted, _ in
+            print("Permission granted: \(granted)")
+            self?.showAlert(granted: granted)
+        }
+    }
+    
+    private func showAlert(granted: Bool) {
+        DispatchQueue.main.async { [weak self] in
+            let alertController = UIAlertController(
+                title: "Permission Status",
+                message: "Permission granted: \(granted)",
+                preferredStyle: .alert
+            )
+            alertController.addAction(UIAlertAction(title: "OK", style: .default))
+            if !granted {
+                alertController.addAction(UIAlertAction(title: "Settings", style: .default) { _ in
+                    if let appSettings = URL(string: UIApplication.openSettingsURLString) {
+                        if UIApplication.shared.canOpenURL(appSettings) {
+                            UIApplication.shared.open(appSettings, options: [:], completionHandler: nil)
+                        }
+                    }
+
+                })
+            }
+            self?.present(alertController, animated: true)
+        }
+    }
 }
 
 // MARK: - WKNavigationDelegate, WKUIDelegate
