b1a98ce8474ba7685169c53fe76984f1b4866aed
Samar Khaled <samar.khaled@gmail.com>
2025-04-21 14:45:05 +0200

    Initial Commit

---
+++ WebViewWithNotificationPermission/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WebViewWithNotificationPermission
+//
+//  Created by Samar Khaled on 21/04/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
